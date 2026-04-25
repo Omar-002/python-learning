f021c6d70a6bdfa1264337582a32b27fcb4c312b
Omar-002 <omario21158@gmail.com>
2026-04-25 14:10:20 +0100

    Create day2_variables.py

---
+++ day2_variables.py
@@ -0,0 +1,40 @@
+# Exercise 1 - profile
+name = "Omar"
+age = 24
+color = "blue"
+print(f"My name is {name}, I am {age} years old, and my favorite color is {color}.")
+
+# Exercise 2 - math with variables
+a = 10
+b = 5
+sum_result = a + b
+difference = a - b
+product = a * b
+print("Sum:", sum_result)
+print("Difference:", difference)
+print("Product:", product)
+
+# Exercise 3 - swap two variables
+word1 = "Hello"
+word2 = "world"
+word1, word2 = word2, word1
+print(word1)
+print(word2)
+
+# Exercise 4 - temperature converter
+c = 100
+f = (c * 9/5) + 32
+print(f"{c}°C is equal to {f}°F")
+
+# Exercise 5 - boolean logic
+# Test 1
+age = 20
+has_ticket = True
+can_enter = (age >= 18) and has_ticket
+print("Test 1:", can_enter)
+
+# Test 2
+age = 16
+has_ticket = True
+can_enter = (age >= 18) and has_ticket
+print("Test 2:", can_enter)
